753f09e44629883138901854a5255abced650c81
Petro Velychko <petro.velychko@nl.abnamro.com>
2017-01-03 10:55:12 +0100

    changed target type from "Timestamp" to "Date"

--- spring-cxf-jpa-example-model/src/main/java/com/vepeter/example/springcxfjpa/model/converter/LocalDateAttributeConverter.java
+++ spring-cxf-jpa-example-model/src/main/java/com/vepeter/example/springcxfjpa/model/converter/LocalDateAttributeConverter.java
@@ -1,6 +1,6 @@
 package com.vepeter.example.springcxfjpa.model.converter;
 
-import java.sql.Timestamp;
+import java.sql.Date;
 
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
@@ -8,15 +8,15 @@ import javax.persistence.Converter;
 import org.joda.time.LocalDate;
 
 @Converter(autoApply = true)
-public class LocalDateAttributeConverter implements AttributeConverter<LocalDate, Timestamp> {
+public class LocalDateAttributeConverter implements AttributeConverter<LocalDate, Date> {
 
     @Override
-    public Timestamp convertToDatabaseColumn(LocalDate localDate) {
-        return localDate == null ? null : new Timestamp(localDate.toDateTimeAtStartOfDay().getMillis());
+    public Date convertToDatabaseColumn(LocalDate localDate) {
+        return localDate == null ? null : new Date(localDate.toDateTimeAtStartOfDay().getMillis());
     }
 
     @Override
-    public LocalDate convertToEntityAttribute(Timestamp dbData) {
+    public LocalDate convertToEntityAttribute(Date dbData) {
         return dbData == null ? null : LocalDate.fromDateFields(dbData);
     }
 
